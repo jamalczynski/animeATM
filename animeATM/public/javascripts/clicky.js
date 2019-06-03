bd1d587d3edffdfa592865dbc6d68216657f637b
jamalczynski <43344539+jamalczynski@users.noreply.github.com>
2019-06-03 00:31:31 -0400

    Add files via upload

---
+++ animeATM/public/javascripts/clicky.js
@@ -0,0 +1,5 @@
+"use strict";
+
+document.getElementById(`language`).addEventListener("click", function(){ alert("You can only use english right now, deal with it."); });
+
+document.getElementById(`subscription`).addEventListener("click", function(){ alert("Your subscription does not need to be renewed at this time."); });
